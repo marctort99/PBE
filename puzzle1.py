e5b389b4a11e698929b3dff29dd83cfa72f27775
marctort99 <79195415+marctort99@users.noreply.github.com>
2021-03-09 18:38:01 +0100

    Add files via upload

---
+++ puzzle1.py
@@ -0,0 +1,17 @@
+import i2c_lib
+import i2c_driver
+import sys 
+
+def main():
+  # Main program block
+  lcd = i2c_driver.lcd()
+  while True:
+    print("Escribe un texto por líneas de 20 caracteres y pulsa Enter seguido de Ctrl+D")
+    msg = sys.stdin.readlines()
+    for x in range(len(msg)):
+        msg = msg[x].replace("\n", "")
+        lcd.lcd_display_string(msg, x+1)
+        
+if __name__ == '__main__':
+    
+    main()
